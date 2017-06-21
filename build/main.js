f89d8135b621b40d3a4fe2a9a8734fdd9e818e04
ferdinand <ferdinand.piette@gmail.com>
2017-06-21 10:35:54 +0200

    adding transpiled code

---
+++ build/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.sensors = exports.Robot = undefined;
+
+var _babelPolyfill = require('babel-polyfill');
+
+var _babelPolyfill2 = _interopRequireDefault(_babelPolyfill);
+
+var _sourceMapSupport = require('source-map-support');
+
+var _Robot = require('./Robot');
+
+var _Robot2 = _interopRequireDefault(_Robot);
+
+var _sensors = require('./sensors');
+
+var _sensors2 = _interopRequireDefault(_sensors);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _sourceMapSupport.install)();
+
+exports.Robot = _Robot2.default;
+exports.sensors = _sensors2.default;
+//# sourceMappingURL=main.js.map
